ad8ab4e3e3ac282815566b2e0bbceaf18f2b0136
Gabriel Cucu <gabriel.c@live.com>
2018-07-14 22:47:24 +0300

    Foundation ZURB
    add multiple packages to make foundationzurb to work properly with js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
 import "./style.scss";
+ 
+
+// import whatInput from 'what-input'
+// // import core foundation files
+// import { foundation } from 'foundation-sites/js/foundation.core';
+// import 'foundation-sites/js/foundation.util.mediaQuery';
+
+// /* import here any additional module */
+
+// // we need to attach the function we exported above to the jQuery object in use in this file
+// $.fn.foundation = foundation;
+import 'script-loader!jquery'
+import 'script-loader!what-input'
+import 'script-loader!foundation-sites'
+
+
+
+// ready to go
+$(document).ready(function() {
+  $(document).foundation();
+  
+});
 console.log("hello, world again 1");
